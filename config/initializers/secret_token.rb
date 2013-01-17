93fb22bd3a8f73fec101b04af57b2afa4cf12c55
Andrey Titar <faust@dot.com.ua>
2013-01-17 07:31:54 +0200

    Init Core

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alanya::Application.config.secret_token = '95b0766be153b1414ced6d3347d1d2900fdeb85461875cfd66fa302036a440999df72ac0b00023af4906e2ffb707f69e1c05252beefa78d79534591036f6847a'
