30fed0824c34637931e6896a2ef62856a67bf2d8
Andrey Titar <faust@dot.com.ua>
2013-03-12 22:49:38 +0200

    Fix slider

--- app/models/slider.rb
+++ app/models/slider.rb
@@ -1,6 +1,7 @@
 class Slider < ActiveRecord::Base
   attr_accessible :data, :model_id, :model_type, :photo
   store :data, accessors: [:title, :price, :description, :link]
+  attr_accessible :title, :price, :description, :link
 
   validates :photo, presence: true
   belongs_to :model, polymorphic: true
