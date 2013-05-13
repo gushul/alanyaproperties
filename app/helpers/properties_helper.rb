c5a10a236b5f9b7179945c8da1ba556f7bc4b206
Andrey Titar <faust@dot.com.ua>
2013-05-13 14:29:59 +0300

    Fix photo helper

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -14,10 +14,10 @@ module PropertiesHelper
 
   def photo_tag(photo, argv)
     style = argv.delete(:style) || :small
-    url = photo.try(:video) || photo.url
+    url = photo.respond_to?(:video) ? photo.video : photo.url
     img = image_tag(photo.send(style), argv)
-    img += image_tag('play_video_mini.png', class: 'play_video') if photo.try(:video)
-    link_to(img, url, title: argv[:title], class: "property_#{photo.try(:video) ? 'video' : 'photo'}")
+    img += image_tag('play_video_mini.png', class: 'play_video') if photo.respond_to?(:video)
+    link_to(img, url, title: argv[:title], class: "property_#{photo.respond_to?(:video) ? 'video' : 'photo'}")
   end
 
   def build_path(property, action = nil)
