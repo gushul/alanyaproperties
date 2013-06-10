934f964542ecfdb05c32ce59c79abcd73c8032a5
Andrey Titar <faust@dot.com.ua>
2013-06-10 16:06:45 +0300

    [Search] h1 and title build. Add description field settings

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -23,4 +23,12 @@ module PropertiesHelper
   def build_path(property, action = nil)
     [property.property_for, property].unshift(action)
   end
+
+  def build_title(options = {})
+    t 'property.title.pattern',
+      for: t("property.title.for.#{options[:property_for]}"),
+      type: options[:property_type].blank? ? t('property.title.type.blank') : options[:property_type].map {|type| t("property.title.type.#{type}") }.join(', '),
+      city: options[:city_id].blank? ? t('property.title.city.blank') : City.find(options[:city_id]).map {|city| city.name_case3 }.join(", "),
+      suffix: options[:to_sea].blank? ? '' : " " << options[:to_sea].map {|dist| t("property.title.to_sea.#{dist}") }.reject(&:blank?).join(', ')
+  end
 end
