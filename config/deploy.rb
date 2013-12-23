bbdddfd9bdb3f6a38eb0b5fc5ca94b28428f69ca
Andrey Titar <faust@dot.com.ua>
2013-12-23 11:31:14 +0200

    Fill sitemap

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,4 +50,8 @@ namespace :deploy do
   end
 end
 
+after "deploy", "refresh_sitemaps"
+task :refresh_sitemaps do
+  run "cd #{latest_release} && RAILS_ENV=#{rails_env} rake sitemap:refresh"
+end
 
