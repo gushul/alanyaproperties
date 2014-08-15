3b695958f30d8ca0b632a24db5e1631514df0a28
Ponomarev Nikolay <itsnikolay@gmail.com>
2014-08-15 13:57:01 +0400

    to_top button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.datepicker
@@ -23,6 +11,7 @@
 //= require transfer
 //= require jquery.colorbox-min
 //= require jquery.expander
+//= require to_top
 
 $(function () {
     window.wiselinks = new Wiselinks($('body'), {html4: false});
