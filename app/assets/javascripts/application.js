abdba53c63992df2d0223725436e2af4de960e95
Ponomarev Nikolay <itsnikolay@gmail.com>
2014-08-15 19:12:09 +0400

    slider autoplay

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,47 +12,48 @@
 //= require jquery.colorbox-min
 //= require jquery.expander
 //= require to_top
+//= require search_button
 
 $(function () {
-    window.wiselinks = new Wiselinks($('body'), {html4: false});
+  window.wiselinks = new Wiselinks($('body'), {html4: false});
 
-    $('.property_photo').colorbox({rel: 'property', maxWidth: '80%', maxHeight: '90%', className: 'property_box'});
-    $('.property_video').colorbox({rel: 'property', innerWidth:500, innerHeight:409, className: 'property_box', iframe: true});
-    $('.property_map').colorbox({iframe: true, width: '80%', height: '90%', className: 'property_box'});
-    $('.send_offer').colorbox({iframe: true, width: '490px', height: '516px', close: 'Закрыть окно', fastIframe: false});
-    $('.main_slider .video').colorbox({iframe:true, innerWidth:500, innerHeight:409});
+  $('.property_photo').colorbox({rel: 'property', maxWidth: '80%', maxHeight: '90%', className: 'property_box'});
+  $('.property_video').colorbox({rel: 'property', innerWidth:500, innerHeight:409, className: 'property_box', iframe: true});
+  $('.property_map').colorbox({iframe: true, width: '80%', height: '90%', className: 'property_box'});
+  $('.send_offer').colorbox({iframe: true, width: '490px', height: '516px', close: 'Закрыть окно', fastIframe: false});
+  $('.main_slider .video').colorbox({iframe:true, innerWidth:500, innerHeight:409});
 
-    $('.live-form').change(function(e){
-        // Wiselinks load
-        // var url = $(this).attr('action') + '?' + $(this).serialize();
-        // console.log(url)
-        // wiselinks.load(url, "#live", 'partial')
-        // Precompilation assets hook
+  $('.live-form').change(function(e){
+    // Wiselinks load
+    // var url = $(this).attr('action') + '?' + $(this).serialize();
+    // console.log(url)
+    // wiselinks.load(url, "#live", 'partial')
+    // Precompilation assets hook
 
 
-       setTimeout(function(){ $('.live-form').submit(); });
-    });
+    setTimeout(function(){ $('.live-form').submit(); });
+  });
 
-    $('div.expander').expander({
-      slicePoint: 510,
-      expandText: 'Читать далее',
-      // expandPrefix: '. ',
-      userCollapseText: 'свернуть..'
-    });
+  $('div.expander').expander({
+    slicePoint: 510,
+    expandText: 'Читать далее',
+    // expandPrefix: '. ',
+    userCollapseText: 'свернуть..'
+  });
 
-    $('.has-tooltip').tooltip({
-			track: true,
-      position: {
-        my: "center top+20",
-        at: "center bottom",
-        using: function( position, feedback ) {
-          $( this ).css( position );
-          $( "<div>" )
-            .addClass( "arrow" )
-            .addClass( feedback.vertical )
-            .addClass( feedback.horizontal )
-            .appendTo( this );
-        }
+  $('.has-tooltip').tooltip({
+    track: true,
+    position: {
+      my: "center top+20",
+      at: "center bottom",
+      using: function( position, feedback ) {
+        $( this ).css( position );
+        $( "<div>" )
+        .addClass( "arrow" )
+        .addClass( feedback.vertical )
+        .addClass( feedback.horizontal )
+        .appendTo( this );
       }
-		});
+    }
+  });
 });
