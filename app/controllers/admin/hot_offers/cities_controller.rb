18ba4d67075f3387bf488598d8a06bba6236e2a7
Ponomarev Nikolay <itsnikolay@gmail.com>
2014-08-18 05:43:18 +0400

    hot offers for cities

---
+++ app/controllers/admin/hot_offers/cities_controller.rb
@@ -0,0 +1,3 @@
+# encoding: utf-8
+class Admin::HotOffers::CitiesController < Admin::HotOffers::BaseController
+end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     @hot_offers_property = HotOffer.properties.published.includes(:offerable).shuffle
+    @hot_offers_cities   = HotOffer.cities.published.includes(:offerable).shuffle
     @properties = Property.hot.order('RAND()').limit(4)
     @sliders = Slider.unscoped.slider_type('main').order('RAND()')
     @settings = Setting.get('main')
