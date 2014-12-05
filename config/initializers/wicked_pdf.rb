ce127fd332aeae09378201b251e5c224e14f4f33
Ponomarev Nikolay <itsnikolay@gmail.com>
2014-12-05 17:05:11 +0300

    fix

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,9 @@
-WickedPdf.config = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
-  #:exe_path => '/usr/local/bin/wkhtmltopdf'
-}
+if Rails.env.production?
+  WickedPdf.config = {
+    #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
+    #:layout => "pdf.html",
+    :exe_path => '/usr/local/bin/wkhtmltopdf'
+  }
+else
+  WickedPdf.config = {}
+end
